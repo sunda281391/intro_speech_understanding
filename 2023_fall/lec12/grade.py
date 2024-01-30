501112f852a4a8f632e654c6a70ee51735e066c5
sunda281391 <148168656+sunda281391@users.noreply.github.com>
2024-01-30 13:38:55 +0900

    hw12

--- 2023_fall/lec12/grade.py
+++ 2023_fall/lec12/grade.py
@@ -68,3 +68,5 @@ result = unittest.TextTestRunner().run(suite)
 n_success = result.testsRun - len(result.errors) - len(result.failures)
 print('%d successes out of %d tests run'%(n_success, result.testsRun))
 print('Score: %d%%'%(int(100*(n_success/result.testsRun))))
+                                
+                    
